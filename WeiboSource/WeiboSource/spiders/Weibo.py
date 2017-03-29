6c6bddac70d7945f00244bc39d49d8ea1a90ab16
qingw <qingw@jumei.com>
2017-03-29 16:22:35 +0800

    1. 修改微博的 cookie; 2. 精简化需要传递的 微博 cookie。

--- WeiboSource/WeiboSource/spiders/Weibo.py
+++ WeiboSource/WeiboSource/spiders/Weibo.py
@@ -10,26 +10,7 @@ class Weibo(CrawlSpider):
     start_urls = ['http://weibo.com/p/10050583018062']  # 要爬取的页面地址
 
     def start_requests(self):
-        cookies = {"SINAGLOBAL": "6109691141173.243.1438072255386",
-                   "_ga": "GA1.2.2073578723.1457689189",
-                   "__gads": "ID",
-                   "YF-Ugrow-G0": "ad06784f6deda07eea88e095402e4243",
-                   "YF-V5-G0": "c37fc61749949aeb7f71c3016675ad75",
-                   "_s_tentry": "login.sina.com.cn",
-                   "Apache": "8319853895847.242.1490687179455",
-                   "ULV": "1490687179779:150:5:1:8319853895847.242.1490687179455:1489573390792",
-                   "YF-Page-G0": "e44a6a701dd9c412116754ca0e3c82c3",
-                   "login_sid_t": "cbafa21194854406fba7980d30b354be",
-                   "UOR": "blog.csdn.net,widget.weibo.com,login.sina.com.cn",
-                   "WBStorage": "02e13baf68409715|undefined",
-                   "SCF": "Aof0G5U0IYtGXakUN5rDtjMpG63xnPsfAGHAI56omax6JTrFNqPjZLKD3aVioESJ-uZqwtyXBHJi4LITefy1nl0.",
-                   "SUB": "_2A2513yOwDeRxGedJ7lIR8ifOzDiIHXVWrRJ4rDV8PUNbmtBeLVjakW86aca01a4BFS9OxcgGLC_iwos-6w..",
-                   "SUBP": "0033WrSXqPxfM725Ws9jqgMF55529P9D9WWdYaRAKqagDyKVOZec8i675JpX5K2hUgL.Fo2NSK57eo.ES0B2dJLoIEBLxK-LB-BLBKBLxKML12zLB-eLxKML12zL1hzLxK.LBK-LB--t",
-                   "SUHB": "0ebcuqHxxLAQ9V",
-                   "ALF": "1522304863",
-                   "SSOLoginState": "1490768864",
-                   "un": "gdky005@126.com",
-                   "wvr": "6"}
+        cookies = {"SUB": "_2A2513xyfDeThGeBP41cV9CvOyjmIHXVWrQlXrDV8PUNbmtBeLXHskW9Kvq37pvWg2BLy07rlZ2AI2zjHhg.."}
         return [FormRequest(self.start_urls[0], cookies=cookies, callback=self.parse)]
 
     def parse(self, response):
